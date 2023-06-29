84b8db35cb18ae8e3bcd2bbe4f9a222d9799b937
VitaliiShpychka <122472658+VitaliiShpychka@users.noreply.github.com>
2023-06-29 11:35:51 +0200

    first

---
+++ SimplCalculator/src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class SumCalculator {
+    public int sum(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be a non-negative integer");
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
